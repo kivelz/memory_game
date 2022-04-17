02328f1fcedd1cd76fee9131b8f6e9304118e680
Kelvin Tay <kelvintay@Kelvins-MacBook-Air.local>
2022-04-17 15:22:11 +0800

    disable console.warn if dev env

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+if (__DEV__) {
 
+    console.log();
+} else {
+    console.log = function () {};
+}
 AppRegistry.registerComponent(appName, () => App);
